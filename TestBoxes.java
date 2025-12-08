6e656f927bfc770d26692525c0a93c289915fe7f
bouabboune <pro.bouabboune@gmail.com>
2025-12-08 12:02:35 -0600

    test thing correct

--- TestBoxes.java
+++ TestBoxes.java
@@ -11,8 +11,41 @@ public class TestBoxes {
     public void testBoxAdd(){
 
         Box b = new Box();
-        b.add("truc1");
-        b.add("truc2");
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        b.add(truc1);
+        b.add(truc2);
     }
+    @Test
+    public void testBoxContains(){
+
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        Thing truc3 = new Thing("truc3");
+        b.add(truc1);
+        b.add(truc2);
+        assertTrue(b.contains(truc1));
+        // doit contenir un truc 1
+        assertTrue(b.contains(truc2));
+        assertFalse(b.contains(truc3));
+        // ne odoit ps contenir truc3
+    }
+    @Test
+    public void testBoxRemove(){
+
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        Thing truc3 = new Thing("truc3");
+        b.add(truc1);
+        b.add(truc2);
+        assertTrue(b.contains(truc1));
+        // doit contenir un truc 1
+        assertTrue(b.contains(truc2));
+        assertFalse(b.contains(truc3));
+        // ne odoit ps contenir truc3
+    }
+ 
 }
-g
+
